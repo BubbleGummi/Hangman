0af708dcc15ce1a36884c8ae283dd763d83f1cef
Diana Hogescu <diana.hogescu@learnet.se>
2023-08-19 23:37:02 +0200

    final touches + test

--- Hangman_Tests/WordGeneratorTest.cs
+++ Hangman_Tests/WordGeneratorTest.cs
@@ -1,6 +1,40 @@
-﻿namespace Hangman_Tests
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Hangman.WordGenerator;
+using Hangman.Models;
+using Newtonsoft.Json;
+
+namespace Hangman.Tests
 {
-    internal class WordGeneratorTest
+    [TestClass]
+    public class RandomWordGeneratorTests
     {
+        private class TestHttpMessageHandler : HttpMessageHandler
+        {
+            protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, System.Threading.CancellationToken cancellationToken)
+            {
+                // Simulate a response with a predefined word
+                var response = new HttpResponseMessage(System.Net.HttpStatusCode.OK)
+                {
+                    Content = new StringContent(JsonConvert.SerializeObject(new RandomWordModel { Word = "testword" }))
+                };
+                return response;
+            }
+        }
+
+        [TestMethod]
+        public async Task GetRandomWord_ReturnsRandomWord()
+        {
+            // Arrange
+            var httpClient = new HttpClient(new TestHttpMessageHandler());
+
+            // Act
+            string result = await Task.Run(() => RandomWordGenerator.GetRandomWord());
+
+            // Assert
+            Assert.AreEqual("testword", result);
+        }
+
     }
 }
